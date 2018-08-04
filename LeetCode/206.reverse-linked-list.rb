7d4fc54ffd697524035f774f0b719dbbe736112b
X140Yu <zhaoxinyu1994@gmail.com>
2018-08-04 19:42:32 +0800

    modified:   206.reverse-linked-list.rb

--- LeetCode/206.reverse-linked-list.rb
+++ LeetCode/206.reverse-linked-list.rb
@@ -11,7 +11,17 @@
 
 # @param {ListNode} head
 # @return {ListNode}
-def reverse_list(head)    
+def reverse_list(head)
+  return head if head.nil? or head.next.nil?
+  node = reverse_list(head.next)
+  node.next.next = node
+  node.next = nil
+  node
+end
+
+# @param {ListNode} head
+# @return {ListNode}
+def reverse_list_iterate(head)    
     last = nil
     while head
       n = head.next
